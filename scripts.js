0027550b22560eac9189451bc04fb90db14471ff
Sabittwa Banerjee <sabittwa.work@gmail.com>
2024-06-24 19:19:43 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.querySelector('.menu').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('active');
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.menu').addEventListener('click', function() {
+      document.querySelector('.quote-phone').classList.toggle('hiddenquote');
+    });
+  });
